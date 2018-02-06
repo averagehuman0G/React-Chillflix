0046f363626e489a40691c120f340bb09772253a
Jonathan Vargas <jonathanv3232@gmail.com>
2018-02-06 00:59:15 -0500

    sets up eslint

--- js/clientApp.jsx
+++ js/clientApp.jsx
@@ -2,7 +2,7 @@ var MyFirstComponent = function() {
   return React.createElement(
     'div',
     null,
-    React.createElement('h1', null, 'This is my first component!')
+    React.createElement('h1', null, 'Just setting up my tooling before I get to Chillflix!')
   );
 };
 ReactDOM.render(
