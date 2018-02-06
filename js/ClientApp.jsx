346f2cd5a2e50fce1cc72edf16c89e4d34b7826d
Jonathan Vargas <jonathanv3232@gmail.com>
2018-02-06 01:55:07 -0500

    sets up babel and webpack

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,11 +1,15 @@
-var MyFirstComponent = function() {
+import React from 'react';
+import { render } from 'react-dom';
+
+const MyFirstComponent = function title() {
   return React.createElement(
     'div',
     null,
     React.createElement('h1', null, 'Just setting up my tooling before I get to Chillflix!')
   );
 };
-ReactDOM.render(
+
+render(
   React.createElement(MyFirstComponent),
   document.getElementById('app')
 );
