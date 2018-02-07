891555ebc0095cfa10bf9544378a2ecbc355d564
Jonathan Vargas <jonathanv3232@gmail.com>
2018-02-07 13:21:33 -0500

    Separates landing page from client app

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const App = () => (
-  <div className="app">
-    <div className="landing">
-      <h1> Introducing JSX </h1>
-    </div>
-  </div>
-);
+const App = () => <div className="app" />;
 
 render(<App />, document.getElementById('app'));
---
+++ js/Landing.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Landing = () => (
+  // Picking a user should go here.
+  <div className="landing">
+    <h1> This is the landing page! </h1>
+  </div>
+);
+
+export default Landing;
