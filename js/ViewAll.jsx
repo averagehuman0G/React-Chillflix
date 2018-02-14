eb598c36fd7cf531dc35a31a4e8f0de40fc669bb
Jonathan Vargas <jonathanv3232@gmail.com>
2018-02-13 20:12:06 -0500

    fixes path to image files for the shows

--- js/ViewAll.jsx
+++ js/ViewAll.jsx
@@ -5,7 +5,7 @@ const ViewAll = () => (
   <div className="viewing-all">
     {data.shows.map(show => (
       <div className="show-card">
-        <img alt={`${show.title} Show Poster`} src={`/public/img/poster/${show.poster}`} />
+        <img alt={`${show.title} Show Poster`} src={`/public/img/posters/${show.poster}`} />
         <div className="show-info">
           <h3>{show.title}</h3>
           <h4>({show.year})</h4>
