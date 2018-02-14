b56a0038afe8fd967ce3bc2ae6f784ad8ad1d599
Jonathan Vargas <jonathanv3232@gmail.com>
2018-02-13 21:21:47 -0500

    adds styled components and styles elements with them

--- js/ViewAll.jsx
+++ js/ViewAll.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
 import ShowCard from './showCard';
 import data from '../data.json';
 
-const ViewAll = () => (
-  <div className="viewing-all">{data.shows.map(show => <ShowCard {...show} key={show.imdbID} />)}</div>
-);
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin: 2% 10%;
+`;
+
+const ViewAll = () => <Wrapper>{data.shows.map(show => <ShowCard {...show} key={show.imdbID} />)}</Wrapper>;
 
 export default ViewAll;
--- js/showCard.jsx
+++ js/showCard.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const ShowCardWrapper = styled.div`
+  flex: 1;
+  justify-content: space-around;
+  width: 25%;
+  height: 500px;
+  border: 1px solid black;
+  margin: 5% 5%;
+`;
+const Img = styled.img`
+  max-width: 200px;
+  max-height: 300px;
+`;
 const ShowCard = props => (
-  <div className="show-card">
-    <img alt={`${props.title} Show Poster`} src={`/public/img/posters/${props.poster}`} />
-    <div className="show-info">
+  <ShowCardWrapper>
+    <Img alt={`${props.title} Show Poster`} src={`/public/img/posters/${props.poster}`} />
+    <div>
       <h3>{props.title}</h3>
       <h4>({props.year})</h4>
       <p>{props.description}</p>
     </div>
-  </div>
+  </ShowCardWrapper>
 );
 
 export default ShowCard;
