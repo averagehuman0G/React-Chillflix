817e43ab4e053f8b4fe3f04ee65c24efd17649c1
Jonathan Vargas <jonathanv3232@gmail.com>
2018-02-13 22:34:04 -0500

    changes the view all into a stateful component in order to use input

--- js/ViewAll.jsx
+++ js/ViewAll.jsx
@@ -10,14 +10,33 @@ const Wrapper = styled.div`
   margin: 2% 5%;
 `;
 
-const ViewAll = () => (
-  <div>
-    <header>
-      <Logo />
-      <input type="text" placeholder="search" />
-    </header>
-    <Wrapper>{data.shows.map(show => <ShowCard {...show} key={show.imdbID} />)}</Wrapper>
-  </div>
-);
+class ViewAll extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: 'Debugging',
+    };
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+  }
+  handleSearchTermChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+  render() {
+    return (
+      <div>
+        <header>
+          <Logo />
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="search"
+          />
+        </header>
+        <Wrapper>{data.shows.map(show => <ShowCard {...show} key={show.imdbID} />)}</Wrapper>
+      </div>
+    );
+  }
+}
 
 export default ViewAll;
