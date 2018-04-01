dff2f6f7b45e9eaa6101f7f6cbba49031921af71
Jonathan Vargas <jonathanv3232@gmail.com>
2018-03-31 23:57:21 -0400

    adds search rendering all shows test

--- js/__tests__/viewAll.test.jsx
+++ js/__tests__/viewAll.test.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import ViewAll from '../ViewAll';
+import ShowCard from '../showCard';
+import data from '../../data.json';
 
 configure({ adapter: new Adapter() });
 
-test('ViewAll renders correctly', () => {
-  const componenent = shallow(<ViewAll />);
-  expect(componenent).toMatchSnapshot();
+describe('Search', () => {
+  it('Should render correctly', () => {
+    const component = shallow(<ViewAll />);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('Should render the correct number of shows', () => {
+    const component = shallow(<ViewAll />);
+
+    // console.log(component);
+    expect(component.find(ShowCard).length).toEqual(data.shows.length);
+  });
 });
